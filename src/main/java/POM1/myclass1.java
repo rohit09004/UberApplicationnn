f6445d92fe654edd438a071a18abc3f7c3385ba4
ROHIT <ROHIT@DESKTOP-8SI0GBB>
2022-08-23 01:28:42 +0530

    stag changes

---
+++ src/main/java/POM1/myclass1.java
@@ -0,0 +1,14 @@
+package POM1;
+
+public class myclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Uber Application Verification 001");
+		System.out.println("Uber Application Verification 002");
+		System.out.println("Uber Application Verification 003");
+		System.out.println("Uber Application Verification 004");
+	}
+
+}
